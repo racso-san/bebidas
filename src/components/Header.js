24d4781dcc02cfc9e4c8a013a4d95ed233a23574
oscar <oll301189@gmail.com>
2020-04-29 14:40:44 -0300

    uno

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <Header className="bg-alert">
+            <h1>Busca recetas de bebidas</h1>
+        </Header>
+     );
+}
+ 
+export default Header;
